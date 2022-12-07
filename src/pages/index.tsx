7fd9d2648045225c02c7c584c11b22f940e39333
dududornelees <eduardo.dsdorneles@gmail.com>
2022-12-07 20:09:11 -0300

    init: início do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Sport Scheduler</title>
+                <meta name="description" content="Sport Scheduler - Faça o agendamento do seu esporte preferido!" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>Sport Scheduler!</h1>
+            </main>
+        </div>
+    );
+}
